953bd687d179f32dfd6fa185f8312c517585ebe6
cdberg20 <63938101+cdberg20@users.noreply.github.com>
2020-10-22 16:01:26 -0400

    Team meeting 10/22/2020
    -Display Certifications
    -Display Certification Records
    -Add new Certification

---
+++ app/public/api/certrecords/memberindex.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'common.php';
+
+$db = DbConnection::getConnection();
+
+$sql = 'SELECT * FROM certifications_records, members Where certifications_records.memberID = members.memberID';
+$vars = [];
+
+if (isset($Get['cert_recordID'])) {
+  $sql = 'SELECT * FROM certifications_records WHERE cert_recordID = ?';
+  $vars = [ $_GET['cert_recordID'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$certifications_records = $stmt->fetchAll();
+
+$json=json_encode($certifications_records, JSON_PRETTY_PRINT);
+
+Header('Content-Type: application/json');
+echo $json;
--- app/public/js/app.certrecords.js
+++ app/public/js/app.certrecords.js
@@ -6,25 +6,42 @@ var app = new Vue({
       cert_recordID: '',
       memberID: '',
       certID:'',
-  	  expiration:''
-    }]
+  	  expiration:'',
+      first_name:'',
+      last_name:''
+    }],
+    currentDate:''
   },
 
   methods: {
 
+
     fetchCertRecords(){
-      fetch('api/certrecords/index.php')
+      fetch('api/certrecords/memberindex.php')
       .then(response => response.json())
       .then(json => {
         this.certRecordList=json;
 
         console.log(this.certRecordList);
       });
-    }
+    },
+
+    date_function(){
+      var currentDate = new Date();
+      var dd = String(currentDate.getDate()).padStart(2, '0');
+      var mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = currentDate.getFullYear();
+
+      currentDate = yyyy + '-' + mm + '-' + dd;
+      // currentDate = currentDate.toJSON(); 
+      console.log(currentDate);
+
+      }
 
 
   },
     created() {
       this.fetchCertRecords();
+      this.date_function();
     },
 })
