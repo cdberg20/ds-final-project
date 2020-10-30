a314baea1b9f7386a259d4ae849154fa45abb126
cdberg20 <63938101+cdberg20@users.noreply.github.com>
2020-10-29 20:59:07 -0400

    Edit Cert and Member

---
+++ app/public/js/editCert.app.js
@@ -0,0 +1,63 @@
+var app = new Vue({
+  el: '#editCertApp',
+//initialize
+  data: {
+    editCertList: [],
+    selectedCertRecord: null,
+    selectedCertRecordID: null
+  },
+
+  methods: {
+
+    fetchAllMembersRecords(){
+      fetch('api/certrecords/memberindex.php')
+      .then(response => response.json())
+      .then(json => {
+        this.editCertList=json;
+
+        console.log(this.editCertList);
+      });
+    },
+
+    updateCertRecord(){
+      fetch('api/certrecords/edit.php', {
+       method:'POST',
+       body: JSON.stringify(this.selectedCertRecord),
+       headers: {
+         "Content-Type": "application/json; charset=utf-8"
+       }
+      })
+      .then(response => response.json() )
+      .then(json => {
+        console.log("Returned from post:", json);
+        this.editCertList.push(json[0]);
+        this.selectedCertRecord = this.editMemberData();
+      });
+
+      console.log("Creating (POSTing)...!");
+      console.log(this.selectedCertRecord);
+   },
+
+   editMemberData() {
+     return {
+       certID:'',
+       expiration:''
+     }
+   },
+   deleteCertRecord(){
+     fetch('api/certrecords/delete.php', {
+      method:'DELETE',
+      body: JSON.stringify(this.selectedCertRecordID),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+   });
+ }
+},
+
+
+
+created() {
+    this.fetchAllMembersRecords();
+}
+})
